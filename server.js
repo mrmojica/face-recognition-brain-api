d9172fc1c3019b10b3bc1617001b254123067f7e
mrmojica <mrmojica510@gmail.com>
2020-02-25 12:05:32 -0800

    fix typo

--- server.js
+++ server.js
@@ -66,7 +66,7 @@ app.post("/signin", (req, res) => {
     req.body.email === database.users[0].email &&
     req.body.password === database.users[0].password
   ) {
-    res.json("succcess");
+    res.json("success");
   } else {
     res.status(400).json("error logging in");
   }

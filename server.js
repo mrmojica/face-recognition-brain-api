586aa361dbd89c172f1cb811204469dfc85010f6
mrmojica <mrmojica510@gmail.com>
2020-02-25 09:16:12 -0800

    notes for creating endpoints

--- server.js
+++ server.js
@@ -2,6 +2,19 @@ const express = require("express");
 
 const app = express();
 
+app.get("/", (req, res) => {
+  res.send("This is working!");
+});
+
 app.listen(3000, () => {
   console.log("app is running on port 3000");
 });
+
+/*
+Notes for endpoints
+/ --> res = this is working
+/signin --> POST success/ fail
+/register --> POST = user
+/profile/:userId --> GET = user (info)
+/image --> PUT --> user (updated user data, count/ranking)
+*/

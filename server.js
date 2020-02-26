72b48fc26b92349b6dcf7172ba069561a9b2715e
mrmojica <mrmojica510@gmail.com>
2020-02-26 10:52:41 -0800

    update db entries count for /image endpoint

--- server.js
+++ server.js
@@ -124,15 +124,15 @@ app.get("/profile/:id", (req, res) => {
 
 app.put("/image", (req, res) => {
   const { id } = req.body;
-  const user = getUserById(id);
 
-  if (user) {
-    user.entries++;
-
-    res.json(getUserById(id).entries);
-  } else {
-    res.status(400).json("user not found");
-  }
+  db("users")
+    .where("id", "=", id)
+    .increment("entries", 1)
+    .returning("entries")
+    .then(entries => {
+      res.json(entries[0]);
+    })
+    .catch(err => res.status(400).json("Error getting entries"));
 });
 
 app.listen(3001, () => {
